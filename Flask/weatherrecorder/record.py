459d49177d0c5c1c26e1267e582622330c7bfaa0
vegardalvsaker <vegaal17@student.uia.no>
2019-04-02 16:26:44 +0200

    Added program to record weather data every 3 hours

---
+++ Flask/weatherrecorder/record.py
@@ -0,0 +1,45 @@
+from sense_hat import SenseHat
+import sqlite3
+import os
+import time
+
+#if os.path.isfile('./cabinhat.db')
+conn = sqlite3.connect('cabinhat.db')
+sense = SenseHat()
+
+c = conn.cursor()
+
+c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='weather'")
+
+if c.fetchone() == None:
+    c.execute(""" CREATE TABLE weather (
+                    type text,
+                    value real,
+                    datetime text
+            )""")
+    conn.commit()
+c.execute("SELECT * FROM weather")
+print(c.fetchall())
+conn.close()
+
+
+def get_dict(weath_type, sense_data):
+    return {'type': weath_type, 'value': sense_data}
+
+while True:
+    temp = sense.get_temperature()
+    humid = sense.get_humidity()
+    pressure = sense.get_pressure()
+    
+    conn = sqlite3.connect('cabinhat.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO weather VALUES (:type, :value, DateTime('now'))", get_dict('Temp', temp))
+    c.execute("INSERT INTO weather VALUES (:type, :value, DateTime('now'))", get_dict('Humid', humid))
+    c.execute("INSERT INTO weather VALUES (:type, :value, DateTime('now'))", get_dict('Pressure', pressure))
+    conn.commit()
+    conn.close()
+    time.sleep(5)
+    
+
+
+
