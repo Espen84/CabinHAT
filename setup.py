35797d05d7ec8b3b15b0652020b7ec7d1b0d725a
vegardalvsaker <vegaal17@student.uia.no>
2019-05-03 18:57:30 +0200

    added flask in setup

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='cabinhat', version='1.0', packages=find_packages())
+setup(
+	name='cabinhat', 
+	version='1.0', 
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False, 
+	install_requires=['flask',]
+)
+
